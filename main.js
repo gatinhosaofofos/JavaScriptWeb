72fae41cd5a83b50049ec390976c05d72e95027c
gato <106260330+gatinhosaofofos@users.noreply.github.com>
2023-10-26 10:44:56 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-document.querySelector('.tecla_aplausos');
+// Aplausos ----------------------------------------------
+function TocaSomAplausos () {
+     document.querySelector('#som_tecla_aplausos').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+listaDeTeclas[0].onclick = TocaSomAplausos
+
+listaDeTeclas[1].onclick = tocaSomVaia
+listaDeTeclas[2].onclick = tocaSomRisada
+
+/images-removebg-preview.png
